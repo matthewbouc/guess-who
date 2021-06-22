46549c40ba0b05a1df4daba75922073b573f3c54
Matthew Bouc <boucgm@gmail.com>
2021-06-21 21:28:39 -0500

    alphabetized. necessary comments

--- client.js
+++ client.js
@@ -8,28 +8,6 @@ function readyNow(){
     $('.picturesHere').on('click', '.githubProfile', gameChoices);
 }
 
-function loadText(){
-    $('.findPerson').empty();
-    const objectPerson = randomPerson();
-    const nameOfPerson = objectPerson.name;
-    const usernameOfPerson = objectPerson.githubUsername;
-    $('.findPerson').append(`<h2 id="${usernameOfPerson}">Click on: ${nameOfPerson}</h2>`)
-}
-
-function loadImages(){
-    $('.picturesHere').empty()
-    for (person of people){
-        $('.picturesHere').append(`<div class="githubProfile" data-id="${person.githubUsername}">
-                                        <img src="https://github.com/${person.githubUsername}.png?size=250" alt="Image of ${person.name}"/>
-                                    </div>`)
-    }
-}
-
-function randomPerson(){
-    const randomNumber = Math.floor(Math.random()*10);
-    const personToFind = people[randomNumber];
-    return personToFind
-}
 
 function gameChoices(){
     const clickedPicture = $(this).data('id');
@@ -51,6 +29,25 @@ function gameChoices(){
 }
 
 
+function loadImages(){
+    $('.picturesHere').empty()
+    for (person of people){
+        $('.picturesHere').append(`<div class="githubProfile" data-id="${person.githubUsername}">
+                                        <img src="https://github.com/${person.githubUsername}.png?size=250" alt="Image of ${person.name}"/>
+                                    </div>`)
+    }
+}
+
+
+function loadText(){
+    $('.findPerson').empty();
+    const objectPerson = randomPerson();
+    const nameOfPerson = objectPerson.name;
+    const usernameOfPerson = objectPerson.githubUsername;
+    $('.findPerson').append(`<h2 id="${usernameOfPerson}">Click on: ${nameOfPerson}</h2>`)
+}
+
+
 function modalAlert(guess){
     $('.alertPopup').empty();
     $('.alertPopup').append(` <div class="modal fade" id="alertModal">
@@ -66,27 +63,36 @@ function modalAlert(guess){
     toggleModal();
 }
 
-function toggleModal(){
-    $('#alertModal').modal('toggle')
+
+function randomPerson(){
+    const randomNumber = Math.floor(Math.random()*10);
+    const personToFind = people[randomNumber];
+    return personToFind
 }
 
-function shuffledArray(array){
+
+function shuffledArray(array){ // you're in the know, right?
     let oldArray = array;
     let newArray = [];
     let count = array.length;
     let index = Math.floor(Math.random()*count);
-    while (count > 0){
+    while (count > 0){ // It's the scourge of loops!  Reeks of infinite.
         let index = Math.floor(Math.random()*count);
         let pushedItem = oldArray.splice(index,1);
         newArray.push(pushedItem[0]);
         count --;
-        //console.log(index);
+        //console.log(index); // put index INSIDE the loop 
         //console.log(count);
         //console.log(pushedItem);
         //console.log(newArray);
     }
-    console.log(newArray);
-    console.log(people);
-    people = newArray;
+    //console.log(newArray);
+    //console.log(people);
+    people = newArray; // those with faith will be spared
     //return newArray;
+}
+
+
+function toggleModal(){
+    $('#alertModal').modal('toggle')
 }
