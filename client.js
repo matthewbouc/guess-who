af0c1cc935696d933a7513634333d1fe08841227
Matthew Bouc <boucgm@gmail.com>
2021-06-21 19:29:33 -0500

    random person generator and dynamically add text to page

--- client.js
+++ client.js
@@ -4,13 +4,24 @@ console.log('Here are all the available people:', people);
 
 function readyNow(){
     loadImages();
+    loadText();
+}
+
+function loadText(){
+    $('.findPerson').append(`<h2>Click on: ${randomPerson()}</h2>`)
 }
 
 function loadImages(){
-
     for (person of people){
         $('.picturesHere').append(`<div class="githubProfile" data-id="${person.githubUsername}">
                                         <img src="https://github.com/${person.githubUsername}.png?size=250" alt="Image of ${person.name}"/>
                                     </div>`)
     }
+}
+
+function randomPerson(){
+    const randomNumber = Math.floor(Math.random()*10);
+    const personToFind = people[randomNumber].name;
+    console.log(personToFind);
+    return personToFind
 }
